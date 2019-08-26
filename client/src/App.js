d3017a52d0ae089b9b99dbaa4d9b58f70354d20f
John <john.i.s.ramos@gmail.com>
2019-08-26 15:08:58 -0400

    Add signup page in route /signup

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
+import SignupPage from "./pages/Signup";
 
 import "./App.css";
 
@@ -11,7 +12,8 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/signup" exact component={SignupPage} />
       </BrowserRouter>
     </MuiThemeProvider>
   );
