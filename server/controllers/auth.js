7ce020a742c8db26018d65b53874972c68712114
Kajen Kirubah <kajenk22@hotmail.com>
2019-08-28 01:39:08 -0400

    added login cont/route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
   const name = req.body.name;
@@ -16,4 +17,39 @@ exports.signup = (req, res, next) => {
       res.status(201).send("user created!");
     })
     .catch(err => console.log(err));
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loggedUser;
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        console.log("user with email doesnt exist");
+        return;
+      }
+      loggedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(equal => {
+      if (!equal) {
+        console.log("password dont match");
+        return;
+      }
+
+      const token = jwt.sign(
+        {
+          email: loggedUser.email,
+          userId: loggedUser._id.toString()
+        },
+        "secretpassword",
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).json({ message: "logged in!", token });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
