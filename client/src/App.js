c35f5fdbe3582dadc16011e3e1b2517506f18150
John <john.i.s.ramos@gmail.com>
2019-08-28 16:16:45 -0400

    Add sign out functionality to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import store from "./store";
 
 import { theme } from "./themes/theme";
 import PrivateRoute from "./components/PrivateRoute";
+import NavBar from "./components/NavBar";
 import LandingPage from "./pages/Landing";
 import SignupPage from "./pages/Signup";
 import LoginPage from "./pages/Login";
@@ -17,25 +18,25 @@ import DashboardPage from "./pages/Dashboard";
 
 import "./App.css";
 
-tokenStorage.deleteAuthToken();
-// if (tokenStorage.getAuthToken()) {
-//   const token = tokenStorage.getAuthToken();
-//   setAuthToken(token);
-//   const decoded = jwtDecode(token);
-//   store.dispatch(setCurrentUser(decoded));
+if (tokenStorage.getAuthToken()) {
+  const token = tokenStorage.getAuthToken();
+  setAuthToken(token);
+  const decoded = jwtDecode(token);
+  store.dispatch(setCurrentUser(decoded));
 
-//   const currentTime = Date.now() / 1000;
-//   if (decoded.exp < currentTime) {
-//     store.dispatch(logoutUser);
-//     window.location.href = "./login";
-//   }
-// }
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser);
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
     <Provider store={store}>
       <MuiThemeProvider theme={theme}>
         <BrowserRouter>
+          <NavBar />
           <Route path="/" exact component={LandingPage} />
           <Route path="/signup" exact component={SignupPage} />
           <Route path="/login" exact component={LoginPage} />
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { withStyles } from "@material-ui/core/styles";
-import { Link } from "react-router-dom";
+import { logoutUser } from "../actions/authActions"; 
 
 const styles = theme => ({
     appbar: {
@@ -18,25 +24,68 @@ const styles = theme => ({
     },
     button: {
         color: "white",
+        margin: theme.spacing(2),
+    },
+    link: {
+        color: "white",
+        textDecoration: "none",
+        margin: theme.spacing(2),
     }
-
 })
 
 const signInLink = React.forwardRef((props, ref) => <Link innerRef={ref} to="/login" {...props} />);
 
-const NavBar = ({ classes }) => {
+const NavBar = ({ classes, auth, logoutUser }) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const handleClick = (e) => setAnchorEl(e.currentTarget);
+    const handleClose = (e) => setAnchorEl(null);
     return (
         <AppBar position="static" className={classes.appbar}>
             <Toolbar>
                 <Typography variant="h6" className={classes.title}>
                     tattoo art
                 </Typography>
-                <Button variant="outlined" color="inherit" className={classes.button} component={signInLink}>
-                    sign in
-                </Button>
+                { !auth.isAuthenticated ? 
+                    <Button variant="outlined" color="inherit" className={classes.button} component={signInLink}>
+                        sign in
+                    </Button>
+                    :
+                    <>
+                        <Link component="button" variant="body2" to="#" className={classes.link}>Discover</Link>
+                        <Link component="button" variant="body2" to="#" className={classes.link}>Messages</Link>
+                        <Link component="button" variant="body2" to="#" className={classes.link}>Notifications</Link>
+                        <Button variant="outlined" color="inherit" className={classes.button}>
+                            Create Contest
+                        </Button>
+                        <Avatar alt={auth.user.email} src="" />
+                        <Button aria-controls="account-menu" aria-haspopup="true" className={classes.button} onClick={handleClick}>
+                            Account &#9660;
+                        </Button>
+                        <Menu
+                            id="account-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                            className="menu"
+                        >
+                            <MenuItem onClick={handleClose}>My Profile</MenuItem>
+                            <MenuItem onClick={() => logoutUser()}>Logout</MenuItem>
+                        </Menu>
+                    </>
+                }
             </Toolbar>
         </AppBar>
     )
 }
 
-export default withStyles(styles)(NavBar);
+const mapStateToProps = ({ auth }) => ({
+    auth,
+});
+
+const enhance = compose(
+    withStyles(styles),
+    connect(mapStateToProps, { logoutUser }),
+);
+
+export default enhance(NavBar);
