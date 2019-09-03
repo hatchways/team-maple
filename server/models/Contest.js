7c01558059c434a561009baa8d0db119634d2edf
Kajen Kirubah <kajenk22@hotmail.com>
2019-09-03 13:43:10 -0400

    updated contest model with timestamps

--- server/models/Contest.js
+++ server/models/Contest.js
@@ -2,32 +2,31 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const contestSchema = new Schema({
-  title: {
-    type: String,
-    required: true
+const contestSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    prize: {
+      type: Number,
+      required: true
+    },
+    images: [{ type: String, required: true }],
+    status: {
+      type: String,
+      required: true
+    },
+    deadline: {
+      type: Date,
+      required: true
+    }
   },
-  description: {
-    type: String,
-    required: true
-  },
-  prize: {
-    type: Number,
-    required: true
-  },
-  images: [{ type: String, required: true }],
-  status: {
-    type: String,
-    required: true
-  },
-  deadline: {
-    type: Date,
-    required: true
-  },
-  dateCreated: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Contest", contestSchema);
