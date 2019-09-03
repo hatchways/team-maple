d5d72f93ab55505be20f2ecd4e44eef2ac01ffb9
Kajen Kirubah <kajenk22@hotmail.com>
2019-09-03 13:21:55 -0400

    added timestamp for submission

--- server/models/Submission.js
+++ server/models/Submission.js
@@ -2,21 +2,24 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const submissionSchema = new Schema({
-  url: {
-    type: String,
-    required: true
+const submissionSchema = new Schema(
+  {
+    url: {
+      type: String,
+      required: true
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    contest: {
+      type: Schema.Types.ObjectId,
+      ref: "Contest",
+      required: true
+    }
   },
-  creator: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  },
-  contest: {
-    type: Schema.Types.ObjectId,
-    ref: "Contest",
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Submission", submissionSchema);
