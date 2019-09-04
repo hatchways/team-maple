a5b87eadff57634b58a508c9e106c0041cf7d063
Kajen Kirubah <kajenk22@hotmail.com>
2019-09-04 19:18:43 -0400

    submission upload v2

--- client/src/components/UpSubmission.js
+++ client/src/components/UpSubmission.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
-import { Button, Paper, Typography, Grid } from "@material-ui/core";
-import { CloudUpload } from "@material-ui/icons";
+import { Button, Paper, Typography, IconButton, Grid } from "@material-ui/core";
+import { CloudUpload, PhotoCamera, FilterNone } from "@material-ui/icons";
 import { withStyles } from "@material-ui/core/styles";
+import Submit from "../pages/Submit";
 
 const styles = theme => ({
   root: {
@@ -16,57 +17,47 @@ const styles = theme => ({
     marginTop: theme.spacing(0)
   },
   button: {
-      backgroundColor: 'black',
-      color: 'white',
-      padding: '10px 20px',
-      margin: '20px'
+    backgroundColor: "black",
+    color: "white",
+    padding: "10px 20px",
+    margin: "20px"
+  },
+  largeIcon: {
+    width: 200,
+    height: 200
   }
 });
 
 export default withStyles(styles)(
   class extends Component {
+    state = {
+      file: null
+    };
+
+    onSelectHandler = event => {
+      this.setState({
+        file: event.target.files[0]
+      });
+    };
+
     render() {
       const { classes } = this.props;
+      const { file } = this.state;
       return (
         <div style={{ margin: "auto", textAlign: "center" }}>
-          <Paper className={classes.paper}>
-            <Grid
-              container
-              direction="column"
-              spacing={4}
-              justify="center"
-              className={classes.grid}
-            >
-              <Grid item sm>
-                <Typography variant="h4" align="center">
-                  Submit design
-                </Typography>
-              </Grid>
-              <Grid item sm>
-                <Button
-                  variant="fab"
-                  sizeLarge
-                  onClick={this.handleToggle}
-                  fullWidth={true}
-                >
-                  <CloudUpload />
-                </Button>
-              </Grid>
-              <Grid item sm>
-                <Typography variant="h5" align="center">
-                  Click to choose a file
-                </Typography>
-              </Grid>
-              <Grid item sm>
-                <Typography variant="body2" align="center">
-                  High resolution images PNG, JPG, GIF
-                </Typography>
-              </Grid>
-            </Grid>
-          </Paper>
-          <Button variant="outlined" component="label" className={classes.button}>
-            Submit
-          </Button>
+          <Grid
+            container
+            direction="column"
+            spacing={4}
+            justify="center"
+            className={classes.grid}
+          >
+            <Submit
+              file={file}
+              classes={classes}
+              onSelect={this.onSelectHandler}
+            />
+          </Grid>
         </div>
       );
     }
