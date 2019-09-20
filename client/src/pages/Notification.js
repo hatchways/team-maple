47aedca520267211400b052d53d620e80bbf2e0d
Kajen Kirubah <kajenk22@hotmail.com>
2019-09-19 21:58:55 -0400

    some styling to notifications page

--- client/src/pages/Notification.js
+++ client/src/pages/Notification.js
@@ -6,12 +6,39 @@ import {
   Container,
   Typography,
   Grid,
-  Button
+  Button,
+  Paper,
+  Badge
 } from "@material-ui/core";
+import { orange } from "@material-ui/core/colors";
 import { withStyles } from "@material-ui/core/styles";
 import { setReadNotification } from "../actions/notificationActions";
 
-const styles = theme => ({});
+const styles = theme => ({
+  title: {
+    margin: theme.spacing(2)
+  },
+  outerPaper: {
+    margin: '2px auto',
+    height: '100vh'
+  },
+  paper: {
+    maxWidth: "60%",
+    margin: "16px auto",
+    padding: theme.spacing(2, 2)
+  },
+  badge: {
+    backgroundColor: orange[400],
+    padding: theme.spacing(1)
+  },
+  gridItem: {
+    borderBottom: "2px solid black",
+    margin: theme.spacing(2),
+    "&:hover": {
+      cursor: "pointer"
+    }
+  }
+});
 
 class Notification extends Component {
   handleClicked = notif => {
@@ -26,20 +53,52 @@ class Notification extends Component {
     console.log("from notification page", notifications);
     return (
       <div>
-        this is notifications:{" "}
-        {notifications &&
-          notifications.map((notif, i) => {
-            return (
-              <div key={i} onClick={() => this.handleClicked(notif)}>
-                <div>message: {notif.message}</div>
-                <div>id: {notif._id}</div>
-                <div>read: {notif.read ? "has been read" : "not read"}</div>
-                <div>priority: {notif.priority}</div>
-                <br></br>
-              </div>
-            );
-          })}
-        <div></div>
+          <Typography
+            variant="h3"
+            component="h5"
+            align="center"
+            className={classes.title}
+          >
+            Notifications
+          </Typography>
+          <Grid container>
+            <Paper className={classes.paper}>
+              {notifications &&
+                notifications.map((notif, i) => {
+                  return (
+                    <Grid
+                      item
+                      key={i}
+                      onClick={() => this.handleClicked(notif)}
+                      className={classes.gridItem}
+                    >
+                      {notif.read ? null : (
+                        <div
+                          style={{ marginLeft: "auto", width: "min-content" }}
+                        >
+                          <Badge
+                            overlap="circle"
+                            badgeContent=" "
+                            variant="dot"
+                            classes={{ badge: classes.badge }}
+                          >
+                              <div />
+                          </Badge>
+                        </div>
+                      )}
+                      <div>id: {notif._id}</div>
+                      <div>message: {notif.message}</div>
+                      <div>
+                        read: {notif.read ? "has been read" : "not read"}
+                      </div>
+                      <div>priority: {notif.priority}</div>
+                      <br></br>
+                    </Grid>
+                  );
+                })}
+              <div></div>
+            </Paper>
+          </Grid>
       </div>
     );
   }
