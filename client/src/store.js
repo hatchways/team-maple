2d42b9817c2c8fbfd64aa16ecc67e7c71a74666f
John <john.i.s.ramos@gmail.com>
2019-09-24 15:04:38 -0400

    Prevent crashing if browser does not have redux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -7,13 +7,14 @@ const initialState = {};
 
 const middleware = [thunk, createSocketMiddleware];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore (
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    composeEnhancers(
+        applyMiddleware(...middleware)
+    ),
 );
 
 export default store;
