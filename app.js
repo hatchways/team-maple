100e3d4308cf6ec622e23083a75fd1ac45fc6969
John <john.i.s.ramos@gmail.com>
2019-09-24 10:11:47 -0400

    Add server sending out client app for non-api requests

--- app.js
+++ app.js
@@ -73,4 +73,13 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
+if (['production', 'ci'].includes(process.env.NODE_ENV)) {
+  app.use(express.static('client/build'));
+  
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
